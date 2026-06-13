178b464e1fb8faea730cf4f4ced9c074c958e4f3
Sina Moradian <sinacsharp@gmail.com>
2026-06-13 23:59:07 +0330

    Add LandingPad prefab and final score calculation
    - Add scoreMultiplier field to the LandingPad with getter

--- Assets/Scripts/Lander.cs
+++ Assets/Scripts/Lander.cs
@@ -33,7 +33,7 @@ public class Lander : MonoBehaviour {
 
 
     private void OnCollisionEnter2D(Collision2D collision) {
-        if(!collision.gameObject.TryGetComponent(out LandingPad landingPad)) {
+        if (!collision.gameObject.TryGetComponent(out LandingPad landingPad)) {
             //Crashed! Landed on Terrain.
             Debug.Log("Crashed! Landed on Terrain.");
             return;
@@ -42,7 +42,7 @@ public class Lander : MonoBehaviour {
 
         float relativeVelocityMagnitude = collision.relativeVelocity.magnitude;
         float safeLandingVelocity = 3.5f;
-        
+
         if (relativeVelocityMagnitude > safeLandingVelocity) {
             //Landed Too Hard!
             Debug.Log("Landed Too Hard!");
@@ -52,7 +52,7 @@ public class Lander : MonoBehaviour {
         float dotVector = Vector2.Dot(Vector2.up, transform.up);
         float minDotVector = 0.9f;
 
-        if(dotVector < minDotVector) {
+        if (dotVector < minDotVector) {
             //Landed at a Bad Angle!
             Debug.Log("Landed at a Bad Angle!");
             return;
@@ -69,7 +69,10 @@ public class Lander : MonoBehaviour {
 
 
         Debug.Log("Landed Successfully!");
-        Debug.Log("landingAngleScore :" + landingAngleScore);
-        Debug.Log("landingSpeedScore :" + landingSpeedScore);
+        Debug.Log("landingAngleScore: " + landingAngleScore);
+        Debug.Log("landingSpeedScore: " + landingSpeedScore);
+
+        int score = Mathf.RoundToInt((landingAngleScore + landingSpeedScore) * landingPad.GetScoreMultiplier());
+        Debug.Log("Final Score: " + score);
     }
 }
